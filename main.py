df9b0744f38fb4db38b8052f8841ddb48508b7c2
dariuszlenart <dariusz@lenart-it.pl>
2025-02-19 18:42:57 +0100

    feat: Player implementation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from player import Player
+
 # Initialize Pygame
 pygame.init()
 
@@ -13,13 +15,20 @@ pygame.display.set_caption("Space Shooter")
 running = True
 clock = pygame.time.Clock()
 
+# Initialize player and sprite group
+player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    all_sprites.update()
 
     screen.fill((0, 0, 0))  # Black background
+    all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(120)  # FPS
 
